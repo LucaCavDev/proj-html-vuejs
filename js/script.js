4493bdfa3ea7a40f0bc8b3b71c25358232060b05
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-12-09 17:39:45 +0100

    inizio a toccare il footer

--- js/script.js
+++ js/script.js
@@ -6,16 +6,39 @@ ottimizzato per schermi di larghezza tra 1500px e 2000px;
 
 */
 // _________________________________________________________
-var app = new Vue ({
-  el: '#app',
+var headerVue = new Vue ({
+  el: '#headerVue',
   data: {
     logoHeader: "./img/finance_logo_1x.png",
     arrayHeaderRight: ['HOME', 'ABOUT', 'SERVICES', 'PACKAGES', 'INSIGHT'],
-    buttonHeaderRight: "CONTACT",
+    buttonHeaderRight: "CONTACT"
   },
   methods: {
 
   }
 
 
+});
+
+var footerVue = new Vue ({
+  el: '#footerVue',
+  data: {
+    logoFooter: "./img/finance_logo_footer-200x131.png",
+    arrayRecentPosts: [
+      '> When is it the right time to sell your company?',
+      '> Personal debt or company debt we explore your options',
+      '> An interview with a so-called business angel'
+    ],
+    arrayServices: [
+      '> SMALL BUSINESS LOAN SERVICES',
+      '> CREDIT RATING ADVICE SERVICES',
+      '> CROWD FUNDING SERVICES'
+    ],
+
+
+
+
+
+
+  }
 })
