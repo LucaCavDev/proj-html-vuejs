81a84d4c519c39ef8cd72c5acc513bdf82b7493f
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-12-09 17:03:00 +0100

    header, e le 3 sezioni successive, completato

--- js/script.js
+++ js/script.js
@@ -2,25 +2,20 @@
 
 Aggiungere Header e Footer con VueJs creando una struttura dati che consenta di inserire i link nelle navbar
 
-
+ottimizzato per schermi di larghezza 1500px o più
 
 */
 // _________________________________________________________
-/*
 var app = new Vue ({
   el: '#app',
   data: {
-    arrayFilm: [],
-    arraySerieTv: [],
-    arrayFlags: ['de', 'en', 'es', 'fr', 'it', 'pt','ru', 'zh', 'ja'],
-    thisSearch: ''
-
+    logoHeader: "./img/finance_logo_1x.png",
+    arrayHeaderRight: ['HOME', 'ABOUT', 'SERVICES', 'PACKAGES', 'INSIGHT'],
+    buttonHeaderRight: "CONTACT",
   },
   methods: {
-
 
   }
 
 
 })
-*/
