b006c72723dbf1725a4afb9d3048cdf071aba4cb
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-12-09 15:10:34 +0100

    inizio

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/*
+
+Aggiungere Header e Footer con VueJs creando una struttura dati che consenta di inserire i link nelle navbar
+
+
+
+*/
+// _________________________________________________________
+/*
+var app = new Vue ({
+  el: '#app',
+  data: {
+    arrayFilm: [],
+    arraySerieTv: [],
+    arrayFlags: ['de', 'en', 'es', 'fr', 'it', 'pt','ru', 'zh', 'ja'],
+    thisSearch: ''
+
+  },
+  methods: {
+
+
+  }
+
+
+})
+*/
