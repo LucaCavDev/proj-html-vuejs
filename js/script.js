7bf3d69f94062337b9c80d8f23540bd386df3920
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-12-10 11:42:50 +0100

    footer in lavorazione e manca sezione 5

--- js/script.js
+++ js/script.js
@@ -24,21 +24,43 @@ var footerVue = new Vue ({
   el: '#footerVue',
   data: {
     logoFooter: "./img/finance_logo_footer-200x131.png",
+    recentPosts: "RECENT POSTS",
     arrayRecentPosts: [
       '> When is it the right time to sell your company?',
       '> Personal debt or company debt we explore your options',
       '> An interview with a so-called business angel'
     ],
+    services: "SERVICES",
     arrayServices: [
       '> SMALL BUSINESS LOAN SERVICES',
       '> CREDIT RATING ADVICE SERVICES',
       '> CROWD FUNDING SERVICES'
     ],
+    latest: "LATEST",
+    uccellinoBlu: 'piccoloUccellodiSuccesso',
+
+    arrayCopyright: [
+      '-',
+      '2020',
+      '|',
+      'Avada Theme by Theme Fusion ',
+      '|',
+      'All Rights Reserved ',
+      '|',
+      'Powered by WordPress'
+    ],
+
+    arrayFooterIcons: [
+      'fa.facebook-f',
+      'fa-twitter',
+      'fa-youtube',
+      'fa-instagram' 
+    ]
 
 
-
-
-
-
+    // <i class="fab fa-facebook-f"></i>
+    //<i class="fab fa-twitter"></i>
+    //<i class="fab fa-youtube"></i>
+    //<i class="fab fa-instagram"></i>
   }
 })
