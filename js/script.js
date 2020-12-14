7e021531b6e661e6824f11f179b746c82395ca55
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-12-14 17:34:39 +0100

    dettagli
    spostate alcune voci di vuejs data in html

--- js/script.js
+++ js/script.js
@@ -36,26 +36,21 @@ var sitoWeb = new Vue ({
     logoFooter: "./img/finance_logo_footer-200x131.png",
     recentPosts: "RECENT POSTS",
     arrayRecentPosts: [
-      '> When is it the right time to sell your company?',
-      '> Personal debt or company debt we explore your options',
-      '> An interview with a so-called business angel'
+      'When is it the right time to sell your company?',
+      'Personal debt or company debt we explore your options',
+      'An interview with a so-called business angel'
     ],
     services: "SERVICES",
     arrayServices: [
-      '> SMALL BUSINESS LOAN SERVICES',
-      '> CREDIT RATING ADVICE SERVICES',
-      '> CROWD FUNDING SERVICES'
+      'SMALL BUSINESS LOAN SERVICES',
+      'CREDIT RATING ADVICE SERVICES',
+      'CROWD FUNDING SERVICES'
     ],
     latest: "LATEST",
-    uccellinoBlu: 'piccoloUccellodiSuccesso',
+    annoCorrente: '2020',
     arrayCopyright: [
-      '-',
-      '2020',
-      '|',
-      'Avada Theme by Theme Fusion ',
-      '|',
-      'All Rights Reserved ',
-      '|',
+      'Avada Theme by Theme Fusion',
+      'All Rights Reserved',
       'Powered by WordPress'
     ],
     arrayFooterIcons: [
